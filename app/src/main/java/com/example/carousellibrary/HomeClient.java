112c5dc44df02986153bb1c94d78b804885c88ba
Ryana Imtiaz <ryanaimtiazalu@gmail.com>
2022-09-21 23:01:19 +0600

    Creating and Modifying Signup Activity

--- app/src/main/java/com/example/carousellibrary/HomeClient.java
+++ app/src/main/java/com/example/carousellibrary/HomeClient.java
@@ -3,6 +3,7 @@ package com.example.carousellibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeClient extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class HomeClient extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_client);
+        //Button furpuppy = (Button) findViewById();
+
     }
 }
--- app/src/main/java/com/example/carousellibrary/SignUp.java
+++ app/src/main/java/com/example/carousellibrary/SignUp.java
@@ -1,13 +1,27 @@
 package com.example.carousellibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
+    private Button signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        signupBtn = findViewById(R.id.button2);
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),HomeClient.class);
+                startActivity(intent);
+            }
+        });
     }
 }
