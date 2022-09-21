db060b71b91a3cb445dcd465bc5f9adf89cea496
Ryana Imtiaz <ryanaimtiazalu@gmail.com>
2022-09-21 19:20:56 +0600

    Testing changing activity and creating button

---
+++ app/src/main/java/com/example/carousellibrary/CarouselView.java
@@ -0,0 +1,13 @@
+package com.example.carousellibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarouselView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carousel_view);
+    }
+}
--- app/src/main/java/com/example/carousellibrary/MainActivity.java
+++ app/src/main/java/com/example/carousellibrary/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.carousellibrary;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button enter = (Button) findViewById(R.id.enterButton);
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToCarouselView = new Intent(MainActivity.this, CarouselView.class);
+                startActivity(goToCarouselView);
+            }
+        });
+
     }
 }
