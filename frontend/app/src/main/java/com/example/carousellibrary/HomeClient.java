9044a4a15cb59817049e88e6785e7516ed7633f2
Ryana Imtiaz <ryanaimtiazalu@gmail.com>
2022-09-30 20:05:15 +0600

    External library for carousel added

--- frontend/app/src/main/java/com/example/carousellibrary/HomeClient.java
+++ frontend/app/src/main/java/com/example/carousellibrary/HomeClient.java
@@ -2,20 +2,53 @@ package com.example.carousellibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel;
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeClient extends AppCompatActivity {
     private Button petFeedBtn;
     private Button inboxBtn;
     private Button findBtn;
+    private Button logoutBtn;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_client);
+
+        SharedPreferences settings = getApplicationContext().getSharedPreferences("localStorage", 0);
+        if (!settings.contains("userUNID")){
+            Intent myIntent = new Intent(HomeClient.this, LoginActivity.class);
+            startActivity(myIntent);
+        }
+
+
+        logoutBtn = (Button) findViewById(R.id.logoutBtn);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                SharedPreferences settings = getApplicationContext().getSharedPreferences("localStorage", 0);
+                SharedPreferences.Editor editor = settings.edit();
+                System.out.println(settings.contains("userUNID"));
+                editor.remove("userUNID");
+                editor.apply();
+                Intent myIntent = new Intent(HomeClient.this, LoginActivity.class);
+                startActivity(myIntent);
+                finish();
+            }
+        });
 //
 //        inboxBtn = findViewById(R.id.button);
 //        inboxBtn.setOnClickListener(new View.OnClickListener() {
